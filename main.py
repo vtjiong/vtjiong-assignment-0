7cd557c013e8e061fd6d2b83104d85ee2d9c2abf
Viriyananda Tjiong <vtjiong@bu.edu>
2024-09-10 18:13:27 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def add_two_numbers(a, b):
+    return a + b
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum_of_numbers = add_two_numbers(num1, num2)
+
+print(f"The sum of {num1} and {num2} is: {sum_of_numbers}")
